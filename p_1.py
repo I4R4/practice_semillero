e0fe9097620311d653f3f5e7ac3e754a078469bf
IARA <andresruedaarias@gmail.com>
2024-10-23 11:56:28 -0500

    se realizo practica de tipo de variables y su interaccion

--- p_1.py
+++ p_1.py
@@ -1,10 +1,13 @@
+#practica basica de programacion variable y sus tipos
 holis = "texto"
 x = 1
 mi_variable = 12.2
 y=34.4
 mi_variable=y+mi_variable
 
-print(holis)
+print("\n la primera variable es de tipo:",holis, ", verificamos de nuevo",type(holis),"\n")
 print(mi_variable)
-holis="cambio"
-print (holis)
+holis=2
+print ("\n Ahora la clase de holis es: ",type(holis),", con un valor de:", holis,'\n')
+
+#

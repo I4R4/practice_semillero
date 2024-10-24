a2e747ead4e2cef31965cacd242b912dbe6498b2
IARA <andresruedaarias@gmail.com>
2024-10-23 22:14:02 -0500

    se usaron modulos random and math, con pruebas

--- p_1.py
+++ p_1.py
@@ -10,4 +10,13 @@ print(mi_variable)
 holis=2
 print ("\n Ahora la clase de holis es: ",type(holis),", con un valor de:", holis,'\n')
 
-#
+#practica de tipo de datos
+import random
+import math as mt
+
+pi=mt.pi
+lista=[5,4,3,2,1]
+random.shuffle(lista)
+my_v=lista[-1]*pi
+
+print("esta es la lista: ",lista,"\neste es el valor de mi variable: ",my_v)

dad7b31e3516c1bde1eace692e78c2ee63631f79
IARA <andresruedaarias@gmail.com>
2024-11-01 16:30:33 -0500

    se actualizo para evitar inderminacion en div and resta

--- PRACTICA_1_OPE_BASIC.py
+++ PRACTICA_1_OPE_BASIC.py
@@ -15,7 +15,7 @@ def div(a,b):
     return print(a/b)
 
 #solicitud de informacion al usuario
-num1=int(input("por favor ingrese los siguientes datos,\nPrimer numero: "))
+num1=int(input("\nPor favor ingrese los siguientes datos,\nPrimer numero: "))
 num2=int(input("Segundo numero: "))
 
 op=int(input("Operaciones dissponibles\n 1.Suma\n 2.Resta\n 3.Multiplicacion\n 4.Division\nIngrese la opcion a ejecutar: "))
@@ -27,4 +27,9 @@ operacion={
     3:multi,
     4:div
 }
-operacion.get(op)(num1,num2)
+#condi para corregir error de valores y la indeterminacion
+if num2>num1 and op==2 :
+    print(" El segundo numero es mayor que el primero, operacion no viable...\n Corriga el segundo numero...")
+elif num2>num1 and op==4 :
+    print(" El segundo numero es mayor que el primero, operacion no viable...\n Corriga el segundo numero...")
+else: operacion.get(op)(num1,num2)
